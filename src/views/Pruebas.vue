c91d7799c9acd748ea4a6b3a75746de525634a3c
horaleon1 <horaleon1@gmail.com>
2019-07-22 23:28:05 -0500

    "Create Pruebas.vue added to router and app"

---
+++ src/views/Pruebas.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+    <button @click="activ">Link 1    > </button><br>
+    <div v-show="activate" class="caja">
+    <h1>hola</h1>    
+    </div>
+    <a href="#">Link 2</a><br>
+    <a href="#">Link 3</a><br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      activate:false,
+    }
+  },
+  methods: {
+    activ(){
+      this.activate = !this.activate
+    }
+  }
+};
+</script>
+
+<style <style scoped>
+.class{
+  margin-bottom: 5px solid black;
+  height: 50px;
+}
+</style>
+
