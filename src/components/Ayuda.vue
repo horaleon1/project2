ec0be4f75084d74cb2081b6c686b8f4e488702f6
horaleon1 <horaleon1@gmail.com>
2019-07-27 22:49:17 -0500

    "almost done, need fix arrows"

--- src/components/Ayuda.vue
+++ src/components/Ayuda.vue
@@ -31,13 +31,17 @@ export default {
   background-color: #f3f4f6;
   height: 900px;
   position:absolute;
+  height: 100%;
 }
 .container h1{
  margin-top: 50px;
  width: 80%;
 }
 .buttonX{
-  font-size: 50px;
+  font-size: 30px;
+  float: left;
+  margin-left: 5%;
+  margin-top: 5%;
 }
 </style>
 
--- src/components/User.vue
+++ src/components/User.vue
@@ -6,20 +6,23 @@
           <li>
             <img v-bind:src="url" height="50px" width="50px" />
           </li>
-          <li>{{ user.results[0].name.firststring }} {{ user.results[0].name.last }}</li>
+          <li>{{ user.results[0].name.first }} {{ user.results[0].name.last }}</li>
           <hr />
           <li>Cuenta</li>
           <li>Historial médico</li>
           <li>Métodos de pago</li>
           <li>Cerrar sesión</li>
-          <li>Ayuda</li>
+          <li>
+            <button @click="ayuda = !ayuda">Ayuda</button>
+          </li>
+          <Ayuda v-show="ayuda" />
           <li class="version">Versión 1.0.0</li>
         </ul>
       </div>
     </div>
+
     <div class="mapContainer">
       <!-- <h3 class="circleLogo">LOGO</h3> -->
-
       <button class="circleMenu" @click="sideMenu = !sideMenu">
         <img src alt />
         <eva-icon name="menu" class="icons" height="30px" width="30px"></eva-icon>
@@ -35,134 +38,117 @@
     <div class="containerTipoUsuario">
       <div class="listaTipoUsuario">
         <ul>
-          <li><button>Para mí</button> </li>
-          <li><button>Circulo Familiar</button> </li>
-          <li><button>Para otra Persona</button> </li>
+          <li>
+            <button @click="forMe = !forMe">Para mí</button>
+          </li>
+          <li>
+            <button>Circulo Familiar</button>
+          </li>
+          <li>
+            <button>Para otra Persona</button>
+          </li>
         </ul>
       </div>
     </div>
+    <!-- ////div (Para mi) -->
 
-    <div class="informacion">
-      <div class="containerInput1">
-        <eva-icon name="search" class="icons"></eva-icon>
-        <input type="text" placeholder="   ¿En dónde es tu emergencia? " class="input1" />
-        <button @click="programado = !programado">
-          <eva-icon name="calendar" class="icons"></eva-icon>
-        </button>
-      </div>
-
-      <div v-show="programado" class="programado">
-        <h4 class="traslado">Traslado <br>
-          programado</h4>
-        <date-picker v-model="date" lang="en" type="date" format="YYYY-MM-dd" placeholder="Date" confirm class="date"></date-picker>
-        <date-picker v-model="time" lang="en" type="time" format="HH:mm p" :minute-step="1" placeholder="Time" confirm class="time"
-        ></date-picker>
-        
-      </div>
-
-      <!-- <div class="containerInput2">
-        <input type="text" placeholder="    Ingresa un hospital" class="input2" />
-        <button @click="sugerencias = !sugerencias">
-          <eva-icon name="activity" height="30px" width="30px" class="icons"></eva-icon>
-        </button>
-      </div> -->
-
-      
-
-      <!-- <div v-show="sugerencias" class="sugerencias">
-        <div class="listaHospitales">
-          <ul>
-            <li>
-              <button>
-                <eva-icon name="camera" class="icons"></eva-icon>
-                <br />Hospital
-                <br />fulano 1
-                <br />
-                <eva-icon name="car" class="icons"></eva-icon>
-                <br />5 Minutos
-              </button>
-            </li>
-            <li>
-              <button>
-                <eva-icon name="camera" class="icons"></eva-icon>
-                <br />Hospital
-                <br />fulano 2
-                <br />
-                <eva-icon name="car" class="icons"></eva-icon>
-                <br />10 Minutos
-              </button>
-            </li>
-            <li>
-              <button>
-                <eva-icon name="camera" class="icons"></eva-icon>
-                <br />Hospital
-                <br />fulano 3
-                <br />
-                <eva-icon name="car" class="icons"></eva-icon>
-                <br />8 Minutos
-              </button>
-            </li>
-          </ul>
+    <div class="paraMi" v-show="forMe">
+      <div class="informacion">
+        <div class="containerInput1">
+          <eva-icon name="search" class="icons"></eva-icon>
+          <input type="text" placeholder="   ¿En dónde es tu emergencia? " class="input1" />
+          <button @click="programado = !programado">
+            <eva-icon name="calendar" class="icons"></eva-icon>
+          </button>
         </div>
-      </div> -->
 
-      <div class="containerInput3">
-        <ul>
-          <!-- <li id="pacienteText"><h4>Paciente</h4></li> -->
-          <li>
-            Bebe
-            <br />
-            <br />0 - 12
-            <br />meses
-          </li>
-          <li>
-            Niño
-            <br />
-            <br />1 - 8
-            <br />años
-          </li>
-          <li>
-            Adulto
-            <br />
-            <br />8 +
-            <br />años
-          </li>
-          <!-- <eva-icon name="person-add" class="icons person" height="30px" width="30px"></eva-icon>            -->
+        <div v-show="programado" class="programado">
+          <h4 class="traslado">
+            Traslado
+            <br />programado
+          </h4>
+          <date-picker
+            v-model="date"
+            lang="en"
+            type="date"
+            format="dd-MM-YY"
+            placeholder="Date"
+            confirm
+            class="date"
+          ></date-picker>
+          <date-picker
+            v-model="time"
+            lang="en"
+            type="time"
+            format="HH:MM p"
+            :minute-step="1"
+            placeholder="Time"
+            confirm
+            class="time"
+          ></date-picker>
+        </div>
 
-        </ul>
-      </div>
+        <div class="buttonInput3" @click="arrowInput3 = !arrowInput3">
+          <button>
+            Paciente 
+            <eva-icon name="arrow-ios-downward" class="icons arrow1" height="30px" width="30px"></eva-icon>
+          </button>
+        </div>
 
-      <!-- <div class="containerInput4">
+        <div class="containerInput3" v-show="arrowInput3">
+          <div class="inputContainer3">
+            <form action>
+              <div class="personTypeDiv">
+                <h4>Bebe 0 - 12 meses</h4>
+                <input type="radio" name="personType" value="baby" />
+              </div>
+              <div class="personTypeDiv">
+                <h4>Niño 1 - 8 años</h4>
+                <input type="radio" name="personType" value="child" />
+              </div>
+              <div class="personTypeDiv">
+                <h4>Adulto + 8 años</h4>
+                <input type="radio" name="personType" value="adult" />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <!-- <div class="containerInput4">
         <input type="text" placeholder="    Síntomas" class="input3"> 
         <eva-icon name="list" class="icons" height="30px" width="30px"></eva-icon>
-      </div> -->
+        </div>-->
 
-      <div class="estadoUsuario">
-        <h3>Estado del Paciente</h3>
-       <div class="listaEstadoUsuario">
-        <ul>
-          <form action="">
-          <input type="radio" name="gender" value="male"> Despierto </input>
-          <input type="radio" name="gender" value="male"> Inconsiente </input>
-          <input type="radio" name="gender" value="male"> Nose </input>
-          </form>
-          <li>Inconsiente</li>
-          <li>Nose</li>
-        </ul>
-       </div>
+        <div class="buttonInput4" @click="arrowInput4 = !arrowInput4">
+          <button>
+            Estado del Paciente 
+            <eva-icon name="arrow-ios-downward" class="icons arrow2" height="30px" width="30px"></eva-icon>
+          </button>
+        </div>
+
+        <div class="estadoUsuario" v-show="arrowInput4">
+          <div class="listaEstadoUsuario">
+            <form action>
+              <input type="radio" name="personState" value="awake" /> Despierto
+              <input type="radio" name="personState" value="unconscious" /> Inconsiente
+              <input type="radio" name="personState" value="dontKnow" /> No sé
+            </form>
+          </div>
+        </div>
+        <button class="pedir">Pedir</button>
       </div>
-
-      <button class="pedir">Pedir</button>
     </div>
   </div>
 </template>
 
 <script>
-import DatePicker from 'vue2-datepicker'
-import { log } from 'util';
+import DatePicker from "vue2-datepicker";
+import Ayuda from "../components/Ayuda.vue";
+
 export default {
-  components:{
-    DatePicker
+  components: {
+    DatePicker,
+    Ayuda
   },
   data() {
     return {
@@ -170,9 +156,13 @@ export default {
       programado: false,
       sideMenu: false,
       user: "",
-      date:'',
-      time:'',
-      url:''
+      date: "",
+      time: "",
+      url: "",
+      ayuda: false,
+      forMe: true,
+      arrowInput3: false,
+      arrowInput4: false
     };
   },
   mounted() {
@@ -184,9 +174,7 @@ export default {
       })
       .catch(err => console.log(err));
   },
-  created() {
-
-  }
+  created() {}
 };
 </script>
 
@@ -213,15 +201,18 @@ export default {
   width: 100%;
   margin-left: 5%;
 }
-.informacion .containerInput1{
- border-bottom: 1px solid lightgrey !important;
- width: 90%;
+.informacion .containerInput1 {
+  border-bottom: 1px solid lightgrey !important;
+  width: 90%;
 }
 
 .input1 {
   width: 75%;
   height: 40px;
   margin: 2% 1%;
+  font-size: 14px;
+  color: grey;
+  font-weight: 200;
 }
 .icons {
   fill: #040acb;
@@ -255,62 +246,16 @@ export default {
   color: white;
   letter-spacing: 1.5px;
   text-align: center;
-  margin: 0% 5%;
   font-size: 20px;
+  margin-top: 5%;
 }
 .pedir:active {
   opacity: 0.7;
 }
-.listaHospitales {
-  width: 100%;
-  margin-bottom: 10%;
-}
-.listaHospitales ul {
-  list-style: none;
-  display: inline;
-}
-.listaHospitales ul li {
-  display: list-item;
-  list-style: none;
-  display: inline-block;
-  margin-left: 5%;
-}
-.listaHospitales ul li:nth-child(2){
-  border-right: 1px solid lightgray;
-  border-left: 1px solid lightgray;
-  padding: 0px 5px;
-}
-.containerInput3 {
-  margin-right: 5%;
-  margin-left: -5%;
-  margin-top: 1%;
-}
-.containerInput3 ul{
-  display: flex;
-}
-/* .containerInput3 h4 {
-  margin-left: 0%;
-  border: 1px solid #e4e4e5;
-  padding: 10px;
-  border-radius: 15px;
-  background-color: #e4e4e5;
-  width: 16%;
-} */
 
-.containerInput3 ul li {
-  display: list-item;
-  list-style: none;
-  display: inline-block;
-  margin-left: 5%;
-  border: 1px solid #e4e4e5;
-  padding: 15px;
-  border-radius: 15px;
-  background-color: #e4e4e5;
-  justify-content: space-around;
+.containerInput3 form input {
 }
-.containerInput3:hover ul:hover li:hover {
-  opacity: 0.5;
-}
+
 .sideMenuContainer {
   position: absolute;
   background-color: #f3f4f6;
@@ -330,20 +275,20 @@ export default {
   margin-top: 20%;
 }
 
-.sideMenu ul hr{
+.sideMenu ul hr {
   width: 100%;
   margin-left: 10%;
   margin-top: 20%;
 }
-.sideMenu ul li img{
+.sideMenu ul li img {
   border-radius: 50%;
 }
-.person{
-position: absolute;
-right: 3%;
-top: 78%;
+.person {
+  position: absolute;
+  right: 3%;
+  top: 78%;
 }
-.input3{
+.input3 {
   width: 70%;
   height: 40px;
   margin: 5% 6%;
@@ -351,45 +296,100 @@ top: 78%;
   background-color: #e4e4e5;
   border: #e4e4e5;
 }
-.containerInput4{
+.containerInput4 {
   margin-top: -5%;
 }
-.programado{
-  margin-left: 10%;
+.programado {
+  margin-left: 5%;
   margin-bottom: 10%;
   width: 100%;
+  margin-top: 5%;
 }
-.date{
+.date {
   margin-bottom: 5%;
 }
-.traslado{
-  color:#040acb;
+.traslado {
+  color: #040acb;
   text-align: right;
   margin-right: 15%;
   position: absolute;
   right: -10%;
 }
 
-.listaTipoUsuario{
-
-}
-.listaTipoUsuario ul{
+.listaTipoUsuario ul {
   list-style: none;
-  margin-left: -10%
+  margin-left: -10%;
 }
-.listaTipoUsuario ul li{
+.listaTipoUsuario ul li {
   display: inline;
 }
-.listaTipoUsuario{
+.listaTipoUsuario {
   border-bottom: 1px solid lightgrey;
   margin: 0% 5%;
 }
-.listaEstadoUsuario ul{
+.listaEstadoUsuario ul {
   list-style: none;
 }
-.listaEstadoUsuario ul li{
+.listaEstadoUsuario ul li {
   display: inline;
   margin-left: 10%;
+}
+.arrowInput3 {
+  margin-right: 12%;
+  position: absolute;
+  display: inline;
+}
+.estadoUsuario h3 {
+  font-size: 14px;
+  color: grey;
+  font-weight: 200;
+  letter-spacing: 0.5px;
+  margin-left: 13%;
+  margin-top: 15%;
+}
+.inputContainer3 {
+  margin-left: 10%;
+}
+.personTypeDiv h4 {
+  font-size: 14px;
+  color: grey;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+}
+.arrowInput3 {
+  border-bottom: 1px solid lightgrey;
+  margin: 0% 5%;
+  width: 90%;
+  margin-bottom: 15%;
+}
+.buttonInput3, .buttonInput4{
+  border-bottom: 1px solid lightgrey;
+  width: 90%;
+  padding-right: -5%;
+}
+.buttonInput3 button, .buttonInput4 button{
+  margin: 5% 12%;
+  font-size: 15px;
+  color: grey;
+  font-weight: 200;
+  letter-spacing: 0.5px;
+}
+.arrow1{
+  /* position: absolute;
+  right: 9%;
+  top: 68.5%; */
+}
+.arrow2{
+}
+.listaEstadoUsuario{
+  margin: 5% 0%;
+  font-size: 15px;
+  color: grey;
+  font-weight: 200;
+  letter-spacing: 0.5px;
+}
+.listaEstadoUsuario form input{
+  margin-left: 5%;
 }
 </style>
 
